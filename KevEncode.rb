470b3af4a841f9e3f60edd115dd7c982cb6d3d61
Tess Myers <tess@tessmyers.com>
2014-03-13 02:23:54 -0400

    A simple encrypter/decrypter

---
+++ KevEncode.rb
@@ -0,0 +1,22 @@
+# encrypt it:
+def encrypt
+  puts "what would you like to encrypt?"
+  text = gets.chomp
+  encrypt = text.split("").collect {|letter| (letter.sum + 5).chr}
+  puts encrypt.join
+end
+encrypt
+
+
+# and now decrypt it!
+def decrypt
+  puts "what would you like to decrypt?"
+  text = gets.chomp
+  decrypt = text.split("").collect {|letter| (letter.sum - 5).chr}
+  puts decrypt.join
+end
+
+
+  
+    
+  
