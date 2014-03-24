b4bdad76dec4e6e03ca27e7225e7e85f93de0ad0
Tess Myers <tess@tessmyers.com>
2014-03-23 22:29:44 -0400

    Replaces a clusterfuck of code with a for loop. Magic\!

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,43 +1,14 @@
-#roman numeral converter, based on the older and simpler system. 
 def convert_to_rmn
 num = gets.chomp.to_i
 romans = []
-  
-  while num != num%1000 do 
-    romans.push("M")
-    num = num - 1000
+hash = {'M'=> 1000, 'D'=> 500, 'C'=> 100, 'L'=> 50, 'X'=> 10, 'V'=> 5, 'I'=> 1}
+
+hash.each do |numeral, value|
+  while num != num%value do 
+    romans.push(numeral)
+    num = num - value
   end
-  
-  while num != num%500 do 
-    romans.push("D")
-    num = num - 500
-  end 
-  
-  while num != num%100 do 
-    romans.push("C")
-    num = num - 100
-  end 
-  
-  while num != num%50 do 
-    romans.push("L")
-    num = num - 50
-  end 
-  
-  while num != num%10 do 
-    romans.push("X")
-    num = num - 10
-  end 
-  
-  while num != num%5 do 
-    romans.push("V")
-    num = num - 5
-  end 
-  
-  while num != num%1 do 
-    romans.push("I")
-    num = num - 1
-  end 
-  
- print romans.join
 end
+print romans.join
+end 
 convert_to_rmn
